42a88a25ba2ed743b66b7b9f5d98dd1fb2a9efc0
Jordan Ibarra <jnibarra02@protonmail.com>
2021-12-07 18:51:14 +0000

    Added a quick little assertion to make sure they work.

--- Week1/Day1/Assign1/development/src/main.cpp
+++ Week1/Day1/Assign1/development/src/main.cpp
@@ -1,7 +1,12 @@
 #include <cstdio>
+#include <cassert>
+
+#define assertm(exp, msg) assert(((void)msg, exp))
 
 int main(int /*argc*/, char** /*argv*/)
 {
 	printf("Hello, world!\n");
+	assertm(2 + 2 == 5, "Checking if asserts are working");
+
 	return 0;
 }
