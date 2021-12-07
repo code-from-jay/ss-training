dd52356791364af8429e77d5311e1f1bd77a26d0
Jordan Ibarra <jnibarra02@protonmail.com>
2021-12-07 18:42:29 +0000

    Updated Dockerfile and spun up a simple C++ program

---
+++ Week1/Day1/Assign1/.profile
@@ -0,0 +1,6 @@
+# Set prompt to be colorful and in the format of 'user@dockerdev:/dir$ '
+PS1='\[\033[01;32m\]\u@dockerdev\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+
+# Enable colors for ls and grep
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
--- Week1/Day1/Assign1/docker_run.sh
+++ Week1/Day1/Assign1/docker_run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+SRC_DIR=$(cd $(dirname "$0") && pwd)
+
 # Spin up the 'dockerdev' image created by docker_build.bat, then enter into bash
-docker run -it dockerdev bash
+docker run -it -v $SRC_DIR/development:/home/development dockerdev bash --init-file "/home/development/.profile"
