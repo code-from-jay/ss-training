beba9fa48b42aed295ae5f748376491647a8e4bf
Jordan Ibarra <jnibarra02@protonmail.com>
2022-01-04 14:53:59 -0500

    Unforget to commit Day9

---
+++ Day9/Assign1/src/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cassert>
+#include <random> // std::default_random_engine, std::uniform_int_distribution
+#include <functional> // std::bind
+#include <chrono> // std::chrono::system_clock
+
+typedef std::chrono::system_clock sys_clock;
+
+class Grade
+{
+public:
+	char grade(const int&& score)
+	{
+		return (4 - (score / 10)) + 'A';
+	}
+};
+
+int main(int argc, char** argv)
+{
+	// Set up a random number gen that's seeded by the current time
+	auto now = sys_clock::now().time_since_epoch().count();
+	std::default_random_engine gen;
+	gen.seed(now);
+	std::uniform_int_distribution<int> dist(10, 40);
+	// Basically alias `dist(gen)` to `roll` to make things simple
+	auto roll = std::bind(dist, gen);
+
+	int n[4] = { roll(), roll(), roll(), roll() };
+
+	Grade grader;
+
+	assert(grader.grade(15) == 'D');
+	assert(grader.grade(28) == 'C');
+	assert(grader.grade(33) == 'B');
+	assert(grader.grade(40) == 'A');
+	for (int i = 0; i < 4; ++i)
+	{
+		int val = n[i];
+		std::cout << "Score = " << val << ": Grade " << grader.grade(std::move(val)) << "\n";
+	}
+
+	return 0;
+}
---
+++ Day9/Assign2/src/main.cpp
@@ -0,0 +1,128 @@
+#include <string>
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+#include <random> // std::default_random_engine, std::uniform_int_distribution
+#include <functional> // std::bind
+#include <chrono> // std::chrono::system_clock
+
+using std::chrono::system_clock;
+
+int roll()
+{
+	static auto now = system_clock::now().time_since_epoch().count();
+	static std::default_random_engine gen{now};
+	static std::uniform_int_distribution<int> dist{1, 100};
+	
+	return dist(gen);
+}
+
+struct Student
+{
+	std::string studentId;
+	int score;
+
+	static bool comparator(Student x, Student y)
+	{
+		return x.score < y.score;
+	}
+};
+
+class MergeSort
+{
+public:
+	void sort(std::vector<Student>& students)
+	{
+		merge_sort(students, 0, students.size()-1);
+	}
+
+private:
+	void merge_sort(std::vector<Student>& students, int left, int right)
+	{
+		if (left < right)
+		{
+			int middle = left + (right - left) / 2;
+			merge_sort(students, left, middle);
+			merge_sort(students, middle+1, right);
+			merge(students, left, middle, right);
+		}
+	}
+
+	void merge(std::vector<Student>& students, int left, int middle, int right)
+	{
+		int leftSliceLen = (middle - left) + 1;
+		int rightSliceLen = (right - middle);
+
+		std::vector<Student> leftSlice
+		{
+			students.begin()+left,
+			students.begin()+left+leftSliceLen
+		};
+		std::vector<Student> rightSlice
+		{
+			students.begin()+middle+1,
+			students.begin()+middle+1+rightSliceLen
+		};
+
+		int i = 0, j = 0, k = left;
+		for (; i < leftSliceLen && j < rightSliceLen; ++k)
+		{
+			if (Student::comparator(leftSlice[i], rightSlice[j]))
+			{
+				students[k] = leftSlice[i];
+				++i;
+			}
+			else
+			{
+				students[k] = rightSlice[j];
+				++j;
+			}
+		}
+
+		for (; i < leftSliceLen; ++i, ++k) // Handle extra elements in leftSlice
+		{
+			students[k] = leftSlice[i];
+		}
+
+		for (; j < rightSliceLen; ++j, ++k)
+		{
+			students[k] = rightSlice[j];
+		}
+	}
+};
+
+int main(int argc, char** argv)
+{
+	std::vector<Student> students =
+	{
+		Student{"Valentine", roll()}, Student{"Ralph", roll()},
+		Student{"Landis", roll()}, Student{"Chesley", roll()},
+		Student{"Shelton", roll()}, Student{"Arick", roll()},
+		Student{"Lyle", roll()}, Student{"Shawn", roll()},
+		Student{"Lacy", roll()}, Student{"Ainsleigh", roll()}
+	};
+
+	std::cout << "Unsorted Students\n-----------------\n";
+	for (const auto& student : students)
+	{
+		std::cout
+			<< "Name: " << student.studentId
+			<< ", Score: " << student.score
+			<< "\n";
+	}
+
+	MergeSort ms;
+	ms.sort(students);
+
+	assert(std::is_sorted(students.begin(), students.end(), Student::comparator));
+
+	std::cout << "\nSorted Students\n-----------------\n";
+	for (const auto& student : students)
+	{
+		std::cout
+			<< "Name: " << student.studentId
+			<< ", Score: " << student.score
+			<< "\n";
+	}
+}
